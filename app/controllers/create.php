eed8ae10bfd63dc3770d883a172ab1ed54fd6333
Hareshkumar Patel <hareshpatel080503@gmail.com>
2025-06-14 22:29:21 +0000

    Updated create.php

--- app/controllers/create.php
+++ app/controllers/create.php
@@ -2,7 +2,17 @@
 
 class Create extends Controller {
 
-    public function index() {		
-	    $this->view('create/index');
+    public function index() {
+        $this->view('create/index');
+    }
+
+    public function register() {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        $user = $this->model('User');
+        $user->create($username, $password);
+        $_SESSION['success'] = "Account created. Please login.";
+        header('Location: /login');
     }
 }
